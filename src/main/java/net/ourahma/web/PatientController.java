2c0eb7206f56c4f4d27e685d2f2c68eef1dc20ad
ourahmamaroua <122113141+ourahma@users.noreply.github.com>
2025-05-19 14:40:24 +0000

    ajout de pagination

--- src/main/java/net/ourahma/web/PatientController.java
+++ src/main/java/net/ourahma/web/PatientController.java
@@ -3,9 +3,12 @@ package net.ourahma.web;
 import lombok.AllArgsConstructor;
 import net.ourahma.entities.Patient;
 import net.ourahma.repository.PatientRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -15,9 +18,19 @@ public class PatientController {
     private PatientRepository patientRepository;
 
     @GetMapping("/index")
-    public String index(Model model){
-        List<Patient> patientList= patientRepository.findAll();
-        model.addAttribute("Listpatients", patientList);
+    public String index(Model model, @RequestParam(name = "page", defaultValue = "0") int page ,
+                        @RequestParam(name = "size", defaultValue = "4") int size){
+        // @RequestParam(name = "page"): on lui dit va chercher un paramètre qui s appemme page
+        // sans faire la pagination
+        //List<Patient> patientList= patientRepository.findAll();
+        // integrer la pagination
+        Page<Patient> pagePatients= patientRepository.findAll(PageRequest.of(page,size));
+        // en utilisant getContent, le contenu de la page est retourné à ce point là est la liste des patients
+        model.addAttribute("Listpatients", pagePatients.getContent());
+        // stocker le nombre de pages
+        model.addAttribute("pages",new int[pagePatients.getTotalPages()]);
+        // stocker la page courante pour la colorier
+        model.addAttribute("currentPage",page);
         return "Patients";
     }
 
